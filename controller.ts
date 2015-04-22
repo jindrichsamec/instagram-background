4f5bce9561806194200118eb525e7a017f7c7bb4
Jindrich Samec <samec@medio.cz>
2015-04-22 21:50:05 +0200

    Transition between background images

--- controller.ts
+++ controller.ts
@@ -6,7 +6,7 @@ module xjs {
 
 	export class InstagramBackgroundCtrl {
 
-		public static $inject = ['$element', '$http', '$timeout'];
+		public static $inject = ['$element', '$http', '$interval'];
 
 		public static USER_ID_PLACEHOLDER = '##USER-ID##';
 		public static CLIENT_ID_PLACEHOLDER = '##CLIENT-ID##';
@@ -20,7 +20,7 @@ module xjs {
 		public constructor(
 			private $element: ng.IAugmentedJQuery,
 			private $http: ng.IHttpService,
-			private $timeout: ng.ITimeoutService) {
+			private $interval: ng.IIntervalService) {
 
 			this.clientId = $element.attr('client-id');
 			this.userId = $element.attr('user-id');
@@ -32,16 +32,32 @@ module xjs {
 			url = url.replace(InstagramBackgroundCtrl.CLIENT_ID_PLACEHOLDER, this.clientId);
 			url = url.replace(InstagramBackgroundCtrl.USER_ID_PLACEHOLDER, this.userId);
 
+			this.$element.css('transition', 'background-image 4s ease-in-out');
+
 			var promise = this.$http.jsonp(url, null);
 			promise.success((response: any) => {
 				var url: string,
 					index: number;
 
-				index = this.random(0, response.data.length);
+				this.setBackground(response);
 
-				url = response.data[index].images.standard_resolution.url;
-				this.$element.css('background-image', 'url(' + url +')');
+				this.$interval(() => {
+					this.setBackground(response);
+				}, 10 * 1000);
+
 			});
+		}
+
+		public setBackground(response: any): void {
+			var index: number = this.random(0, response.data.length),
+				url: string = response.data[index].images.standard_resolution.url,
+				image: HTMLImageElement = new Image(),
+				that = this;
+
+			image.onload = function() {
+				that.$element.css('background-image', 'url(' + this.src +')');
+			}
+			image.src = url;
 		}
 
 		public random(min: number, max: number): number {
